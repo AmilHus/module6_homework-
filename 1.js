73573b383236febceb3b0536194e4b99804bc108
AmilHus <46779076+AmilHus@users.noreply.github.com>
2022-02-11 04:01:30 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,30 +1,32 @@
-let arr = [3,3,3,4,0,{},"!"];
-let odd = 0;
-let even = 0;
-let x = 0
-let y = 0
-function CheckNumbers(){
+function CheckNumbers(
+    arr,
+    odd = 0,
+    even = 0,
+    x = 0,
+    y = 0)
+    {
     arr.forEach(element => {
-    if (typeof (element) == "number"){
-        if (element % 2 == 0 && element != 0){
-            even++;  
+        if (typeof (element) == "number"){
+            if (element % 2 == 0 && element != 0){
+                even++;  
+            }
+            else if (element % 2 != 0){
+                odd++;
+            }
+            else if (isNaN.element){
+                y++;
+            }
+            else {
+                x++;
+            }
         }
-        else if (element % 2 != 0){
-            odd++;
-        }
-        else if (isNaN.element){
+        else{
             y++;
-        }
-        else {
-            x++;
-        }
-    }
-    else{
-        y++;
-    }
-})}
-CheckNumbers();
-console.log("Нечетные числа:" + odd);
-console.log("Четные числа:" + even);
-console.log("Нулевые значения:" + x);
-console.log("Не числовые значения:" + y);
+        }   
+    });
+    console.log("Нечетные числа:" + odd);
+    console.log("Четные числа:" + even);
+    console.log("Нулевые значения:" + x);
+    console.log("Не числовые значения:" + y);
+}
+CheckNumbers([3,3,3,4,0,{},"!"]);
